7922493559ca4c8da9c0b9384b132325abbf2c38
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-07-04 20:28:05 +0700

    Add createNotice function

--- public/chat/chat.js
+++ public/chat/chat.js
@@ -1,1 +1,17 @@
-const socket = io();
+$(function() {
+  // eslint-disable-next-line no-undef
+  function createNotice(message) {
+
+    const msg = $('<p>').text(message)
+    const noticeDiv = $('<div>').addClass('notice').append(msg)
+    
+    $('.chats').append(noticeDiv)
+  }
+  const socket = io();
+  
+  socket.on('message', (message) => {
+    console.log(message)
+    createNotice(message)
+  })
+  
+})
