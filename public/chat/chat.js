b56142013768f318396690099f0ae7a291853d13
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-07-04 20:53:21 +0700

    Add createChatOwn function

--- public/chat/chat.js
+++ public/chat/chat.js
@@ -7,6 +7,19 @@ $(function() {
     
     $('.chats').append(noticeDiv)
   }
+
+  function createChatOwn(message) {
+
+    const chatContainer = $('<div>').addClass('chat own').append([
+      $('<div>').addClass('chat-name').append(
+        $('<p>').addClass('username').text('You')
+      ),
+      $('<p>').addClass('chat-message').text(message)
+    ])
+
+    $('.chats').append(chatContainer)
+  }
+
   const socket = io();
   
   socket.on('message', (message) => {
