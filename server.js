a156f41479da93a94ec833ee628f2fe6518f9445
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-07-04 19:34:39 +0700

    Initialize server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+
+const app = express()
+app.use(express.static(path.join(__dirname, 'public')))
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on 127.0.0.1:${PORT}`)
+})
