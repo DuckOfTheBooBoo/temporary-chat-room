ec79fa9ecbdcd1d49cadde6668d7fae378347bbb
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-07-05 14:38:11 +0700

    Override form submission

---
+++ public/index/index.js
@@ -0,0 +1,32 @@
+$(function() {
+
+  function generateRandomString() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let randomString = '';
+  
+    for (let i = 0; i < 10; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      randomString += characters.charAt(randomIndex);
+    }
+  
+    return randomString;
+  }  
+
+  $('#form-field').on('submit', function(event) {
+    event.preventDefault()
+
+    const formData = new FormData(event.target)
+    const data = {}
+
+    for (const [key, val] of formData.entries()) {
+      data[key] = val
+    }
+    if (!data.roomid) {
+      data.roomid = generateRandomString()
+    }
+
+    window.location = `/chat?username=${data.username}&roomid=${data.roomid}`
+  })
+
+
+})
