83cb0a2b9700578b2d3549a57f97dcbfac2b235d
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-07-07 16:07:33 +0700

    Add rooms utils

---
+++ src/utils/rooms.js
@@ -0,0 +1,59 @@
+let rooms = []
+
+const addRoom = (roomid, maxUsers, videoCall, users = []) => {
+  const room = rooms.find(room => room.id === roomid)
+  if (!room) {
+    const config = {
+      id: roomid,
+      maxUsers,
+      videoCall,
+      users
+    }
+    console.log('Creating room with config: ', config) 
+    rooms.push(config)
+  } else {
+    throw new Error('Room exists')
+  }
+}
+
+const addUserToRoom = (roomid, user) => {
+  const room = rooms.find(room => room.id === roomid)
+
+  if (!room) {
+    throw new Error('Room not found')
+  }
+
+  if (room.users.length === room.maxUsers) {
+    throw new Error('Room is full')
+  }
+  room.users.push(user)
+}
+
+const removeUserInRoom = (roomid, user) => {
+  const room = rooms.find(room => room.id === roomid)
+
+  if (!room) {
+    throw new Error('Room not found')
+  }
+
+  rooms.forEach(room => {
+    room.users = room.users.filter(roomUser => roomUser.id !== user.id)
+  })
+
+}
+
+const deleteRoom = (roomid) => {
+  rooms = rooms.filter(room => room.id !== roomid)
+}
+
+const getRooms = () => {
+  return rooms
+}
+
+module.exports = {
+  addRoom,
+  addUserToRoom, 
+  removeUserInRoom,
+  deleteRoom,
+  getRooms
+}
