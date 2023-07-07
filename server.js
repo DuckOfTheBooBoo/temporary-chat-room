dfb2938c995ce836c3cc72cbfe9c7d1185e3a62c
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-07-07 19:54:03 +0700

    Add api router

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const {createServer} = require('http')
 const path = require('path')
 const indexRouter = require('./src/routes/index.route')
 const chatRouter = require('./src/routes/chat.route')
+const apiRouter = require('./src/routes/api.route')
 const socketIoServer = require('./src/services/socketService')
-
 
 const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
 app.use('/', indexRouter)
 app.use('/chat', chatRouter)
+app.use('/api', apiRouter)
 
 const server = createServer(app)
 const io = socketIoServer(server)
