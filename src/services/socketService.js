7ff94d0053b2c94438a859e9ec3b99afb2530672
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-07-11 10:28:30 +0700

    Throw error to client if joining room unsuccessful

--- src/services/socketService.js
+++ src/services/socketService.js
@@ -8,7 +8,18 @@ const socketIoServer = (httpServer) => {
   io.on('connection', (socket) => {
     socket.on('joinRoom', ({username, roomid}) => {
       const user = userJoin(socket.id, username, roomid)
+
+      try {
+        addUserToRoom(roomid, user)
       socket.join(user.roomid)
+      } catch (error) {
+        console.error(error)
+
+        // Emit error message to client
+        socket.emit('errconn', error.message)
+        // Disconnect socket
+        socket.disconnect()
+      }
       
       socket.emit('notice', formatMessage('notice', 'Welcome to the chat'))
       socket.broadcast.to(user.roomid).emit('notice', formatMessage('notice', `${user.username} has joined`))
@@ -21,6 +32,7 @@ const socketIoServer = (httpServer) => {
         const user = userLeave(socket.id)
         if (user) {
           io.to(user.roomid).emit('notice', `${user.username} has leave the chat`)
+          removeUserFromRoom(roomid, user)
         }
       })
     })
