7f6475eb9af6c6416023c99588e279bbaf29abf4
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-07-11 10:54:06 +0700

    Return different layout based on config and device

--- src/controllers/chatPage.controller.js
+++ src/controllers/chatPage.controller.js
@@ -1,4 +1,21 @@
-const {join} = require('path')
+const { isVideoCall } = require("../utils/rooms")
+
+const detectMobile = (userAgent) => {
+  const toMatch = [
+    /Android/i,
+    /webOS/i,
+    /iPhone/i,
+    /iPad/i,
+    /iPod/i,
+    /BlackBerry/i,
+    /Windows Phone/i
+  ]
+
+  return toMatch.some((toMatchItem) => {
+    return userAgent.match(toMatchItem)
+  })
+
+}
 
 module.exports = (req, res) => {
 
@@ -7,5 +24,14 @@ module.exports = (req, res) => {
     return res.status(400).send('<h1>400 Bad Request</h1>')
   }
 
-  return res.render('pages/chat-videocall')
+  let isMobile = detectMobile(req.get('User-Agent'))
+  
+  if (isVideoCall(roomid)) {
+    if (isMobile) {
+      return res.render('pages/chat-videocall-mobile')
+    }
+    return res.render('pages/chat-videocall')
+  }
+
+  return res.render('pages/chat')
 }
