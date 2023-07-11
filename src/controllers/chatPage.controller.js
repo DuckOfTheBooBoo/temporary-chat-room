21524efe2e2a270943b59a5200a398a6b23f524f
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-07-11 10:23:16 +0700

    Display 400 bad request if no query provided

--- src/controllers/chatPage.controller.js
+++ src/controllers/chatPage.controller.js
@@ -1,7 +1,11 @@
 const {join} = require('path')
 
 module.exports = (req, res) => {
-  return res.render('pages/chat-videocall', {
-    roomid: 'Haloo'
-  })
+
+  const {username, roomid} = req.query
+  if (username === undefined || roomid === undefined) {
+    return res.status(400).send('<h1>400 Bad Request</h1>')
+  }
+
+  return res.render('pages/chat-videocall')
 }
